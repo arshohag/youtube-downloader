a16e051be7d774ee1c8459cf02d14244cdfb5146
Ashiqur Rahman <arshohag8@gmail.com>
2022-07-02 01:38:12 +0600

    Add | Youtube Downloader Script using yt-dlp

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use YoutubeDl\Options;
+use YoutubeDl\YoutubeDl;
 
 class FormController extends Controller
 {
@@ -13,6 +15,39 @@ class FormController extends Controller
 
     public function download(Request $request)
     {
-        return view('home');
+        $yt = new YoutubeDl();
+
+        $yt->setBinPath('/usr/local/bin/yt-dlp');
+        $yt->setPythonPath('/usr/local/bin/python3');
+
+        // $yt->onProgress(static function (?string $progressTarget, string $percentage, string $size, string $speed, string $eta, ?string $totalTime): void {
+        //     echo "Download file: $progressTarget; Percentage: $percentage; Size: $size";
+        //     if ($speed) {
+        //         echo "; Speed: $speed";
+        //     }
+        //     if ($eta) {
+        //         echo "; ETA: $eta";
+        //     }
+        //     if ($totalTime !== null) {
+        //         echo "; Downloaded in: $totalTime";
+        //     }
+        // });
+
+        $collection = $yt->download(
+            Options::create()
+                ->downloadPath(public_path('/downloads'))
+                ->url($request->url)
+        );
+
+        foreach ($collection->getVideos() as $video) {
+            if ($video->getError() !== null) {
+                echo "Error downloading video: {$video->getError()}.";
+            } else {
+                echo "Download Success!! <br><br> Title - ".$video->getTitle()."<br><br><br>";
+                echo "Folder - ".$video->getFile();
+
+                echo "<br><br><a href='/'>Download another</a>";
+            }
+        }
     }
 }
